0738b4979ac60ebab4f16575abe265a6d9fd27f7
anbiabi <anbiabi@yahoo.fr>
2026-01-28 22:38:58 +0900

    Remove large files from Git index

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"rr9WTiuQiGDR+m+zEBO/eiRbsMCNZ7/TCxwmLXnqlj0=\"\n}"
--- .next/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__2692d1fb._.css",
-  "static/chunks/_3dadac3e._.js"
-],
-    source: "dynamic"
-});
